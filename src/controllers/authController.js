041b221e087141d4a237d0503311c2fdce567c4c
marlleyck <marlleyck@hotmail.com>
2022-11-22 22:20:39 -0300

    feat: add verify token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -105,7 +105,7 @@ router.post('/auth/user', async (req, res) => {
 })
 
 // Private Route - Profile
-router.get('/user/:id', async (req, res) => {
+router.get('/user/:id', checkToken, async (req, res) => {
     const id = req.params.id
 
     // Check if user exists
@@ -117,5 +117,24 @@ router.get('/user/:id', async (req, res) => {
 
     return res.send(user)
 })
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).send({ error: 'Access denied!' })
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+    } catch(e) {
+        return res.status(400).send({ error: 'Invalid token!' })
+    }
+}
 
 module.exports = router
